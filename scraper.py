1409637bbfd0035ffbceed8f8cf174546fa0cb21
Mourtallah Faye <serigne.faye@icloud.com>
2021-02-24 20:26:16 -0500

    Delete scraper.py

--- scraper.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-url="https://en.wikipedia.org/wiki/Category:2016_California_ballot_propositions"
-page=requests.get(url)
-soup=BeautifulSoup(page,"html_parser")
-print(soup)
